dd5e36635da9ff1b0e78431065d4cf6441453d29
eliyahu <eliyahu.p15@gmail.com>
2021-12-01 09:32:07 +0200

    update

--- Assets/scripts/GameManger.cs
+++ Assets/scripts/GameManger.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using UnityEditor.Experimental.SceneManagement;
-using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
